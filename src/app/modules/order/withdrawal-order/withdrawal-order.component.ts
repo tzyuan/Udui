bf0886f7fce0e7928d156a5d4382206636313608
tzyuan <550354143@qq.com>
2023-06-29 00:07:22 +0800

    体现订单

--- src/app/modules/order/withdrawal-order/withdrawal-order.component.ts
+++ src/app/modules/order/withdrawal-order/withdrawal-order.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { NzModalService } from 'ng-zorro-antd/modal';
 
 @Component({
   selector: 'app-withdrawal-order',
   templateUrl: './withdrawal-order.component.html',
   styleUrls: ['./withdrawal-order.component.scss']
 })
-export class WithdrawalOrderComponent {
+export class WithdrawalOrderComponent implements OnInit {
 
+  orderData: any[] = [];
+  showOrderData: any[] = [];
+  loading = false;
+  one_code = '';
+  constructor(
+    private http: HttpClient,
+    private modal: NzModalService,
+    private message: NzMessageService,
+  ) {
+
+  }
+
+  getData = () => {
+    this.loading = true;
+    this.http.get<any>('/admin/cash-orders').subscribe({
+      next: (res) => {
+        this.loading = false;
+        this.orderData = res
+      }
+    })
+  }
+  deal = (order: any, telContent: TemplateRef<{}>) => {
+    this.one_code = '';
+    this.modal.create({
+      nzTitle: '请输入驳回原因',
+      nzContent: telContent,
+      nzOkDanger: true,
+      nzOkText: '驳回',
+      nzOnOk: () => {
+        if (this.one_code.trim() == '') {
+          this.message.warning('请输入谷歌验证码')
+          return false;
+        }
+        return new Promise((resolve, reject) => {
+          this.http.post('/admin/recharge-order/check', {
+            cash_order_no: order.cash_order_no,
+            one_code: this.one_code
+          }).subscribe({
+            next: (res) => {
+              this.message.success('确认成功');
+              resolve(true)
+            },
+            error: (err) => {
+              reject(false)
+            }
+          })
+        })
+      }
+    });
+
+    this.loading = true;
+    this.http.post(`/cash-order/deal`, {
+      cash_order_no: '',
+      one_code: ''
+    });
+  }
+
+  ngOnInit(): void {
+    this.getData();
+  }
 }
