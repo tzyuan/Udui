ad7183ceb3dce230b9999aca804c12cd9369303f
tzyuan <550354143@qq.com>
2023-07-06 19:54:57 +0800

    banner 增删改查

--- src/app/modules/app/banner-form/banner-form.component.ts
+++ src/app/modules/app/banner-form/banner-form.component.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { NzDrawerRef } from 'ng-zorro-antd/drawer';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { NzUploadChangeParam } from 'ng-zorro-antd/upload';
 
 @Component({
   selector: 'app-banner-form',
@@ -9,23 +11,71 @@ import { NzDrawerRef } from 'ng-zorro-antd/drawer';
   styleUrls: ['./banner-form.component.scss']
 })
 export class BannerFormComponent implements OnInit {
+  @Input() editData: any = null;
+  @Input() showDetail: boolean = false;
   constructor(
     private http: HttpClient,
     private drawerRef: NzDrawerRef<string>,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private message: NzMessageService
   ) { }
-
+  loading = false;
   bannerForm = this.fb.group({
     img_url: [null, [Validators.required]],
     content: [null, [Validators.required]],
   });
+
   fileList: any[] = [];
+  uploadChange = (e: NzUploadChangeParam) => {
+    if (e.type === 'success') {
+      this.bannerForm.get('img_url')?.setValue(e.file.response.img_url);
+    }
+    if (e.type === 'error') {
+      this.message.error('文件上传失败,请重新上传');
+      this.bannerForm.get('img_url')?.setValue(null);
+    }
+  }
 
+  removeFile = () => {
+    this.bannerForm.get('img_url')?.reset();
+  }
   _submitForm = () => {
-
+    if (this.bannerForm.value.img_url == null) {
+      this.message.error('请上传图片');
+      return;
+    }
+    if (this.bannerForm.value.content == null || `${this.bannerForm.value.content}`.trim() == '') {
+      this.message.error('请填写内容');
+      return;
+    }
+    this.loading = true;
+    if (this.editData == null) {
+      this.http.post('/admin/banners', this.bannerForm.value).subscribe(res => {
+        this.message.success('保存成功');
+        this.drawerRef.close(true);
+        this.loading = false;
+      });
+    } else {
+      this.http.patch(`/admin/banners/${this.editData.id}`, this.bannerForm.value).subscribe(res => {
+        this.message.success('保存成功');
+        this.drawerRef.close(true);
+        this.loading = false;
+      });
+    }
   }
   ngOnInit(): void {
-
+    if (this.editData != null) {
+      this.bannerForm.setValue({
+        img_url: this.editData.img_url,
+        content: this.editData.content
+      });
+      this.fileList = [{
+        uid: '-1',
+        name: this.editData.img_url,
+        status: 'done',
+        url: this.editData.img_url,
+      }]
+    }
   }
 
 }
--- src/app/modules/app/banner-setting/banner-setting.component.ts
+++ src/app/modules/app/banner-setting/banner-setting.component.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { NzDrawerService } from 'ng-zorro-antd/drawer';
 import { BannerFormComponent } from '../banner-form/banner-form.component';
+import { NzModalService } from 'ng-zorro-antd/modal';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'app-banner-setting',
@@ -11,7 +13,10 @@ import { BannerFormComponent } from '../banner-form/banner-form.component';
 export class BannerSettingComponent implements OnInit {
   constructor(
     private http: HttpClient,
-    private drawer: NzDrawerService
+    private drawer: NzDrawerService,
+    private modal: NzModalService,
+    private message: NzMessageService
+
   ) { }
   page = {
     index: 1,
@@ -37,12 +42,56 @@ export class BannerSettingComponent implements OnInit {
     this.getData();
   }
   add = () => {
-    this.drawer.create({
+    const drawer = this.drawer.create({
       nzTitle: '新建Banner公告',
       nzContent: BannerFormComponent,
       nzWidth: '80%',
       nzMaskClosable: false,
       nzKeyboard: false
+    });
+
+    drawer.afterClose.subscribe(res => {
+      if (res) { this.getData() }
+    })
+  }
+
+  edit = (i: any) => {
+    const drawer = this.drawer.create({
+      nzTitle: '编辑Banner公告',
+      nzContent: BannerFormComponent,
+      nzContentParams: { editData: i },
+      nzWidth: '80%',
+      nzMaskClosable: false,
+      nzKeyboard: false
+    });
+
+    drawer.afterClose.subscribe(res => {
+      if (res) { this.getData() }
+    })
+  }
+
+  del = (i: any) => {
+    this.modal.confirm({
+      nzTitle: '确定删除吗?',
+      nzContent: '删除后数据将无法恢复',
+      nzOnOk: () => {
+        return new Promise(resolve => {
+          this.http.delete(`/admin/banners/${i.id}`).subscribe(res => {
+            resolve(true);
+            this.message.success('删除成功');
+            this.getData();
+          })
+        })
+      }
+    })
+  }
+
+  detail = (i: any) => {
+    this.drawer.create({
+      nzTitle: '查看Banner公告',
+      nzContent: BannerFormComponent,
+      nzContentParams: { editData: i, showDetail: true },
+      nzWidth: '80%',
     });
   }
   ngOnInit(): void {
